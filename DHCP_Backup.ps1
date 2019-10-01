0167f13fefa923b0c543ceb455792c6ebb7aa81f
Arjun Nair <36537512+arjunnairp@users.noreply.github.com>
2019-10-01 17:28:07 +0530

    v2 - Major Change
    Detects dhcp interval and backup location from registry. Near 0 Manual intervention

--- DHCP_Backup.ps1
+++ DHCP_Backup.ps1
@@ -2,7 +2,9 @@
 # Script_Name : DHCP_Backup.ps1
 # Description : Create a subfolder inside %systemroot%\system32\dhcp\backup and copy the dhcp backup files to it.
 # Requirements : Windows 2008/R2 + DNS Management console Installed
-# Version : 1
+# Version : 2
+# Changes:
+# v2: Detects dhcp interval and backup location from registry. Near 0 Manual intervention  - Oct 2019
 # Date : January 2019
 # Created by Arjun N
 # Disclaimer:
@@ -12,6 +14,25 @@
 # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
 # OUT OF OR IN CONNECTION WITH THE SCRIPT OR THE USE OR OTHER DEALINGS IN THE SCRIPT.
 #——————————————————————————————-#
+$ErrorActionPreference = "Stop"
+$key = 'HKLM:\SYSTEM\CurrentControlSet\Services\DHCPServer\Parameters'
+$bkpint = (Get-ItemProperty -Path $key -Name BackupInterval).BackupInterval
+$Bkppth = (Get-ItemProperty -Path $key -Name BackupDatabasePath).BackupDatabasePath
+
+$backup = {
 $FileName = "sched_"+(Get-Date).tostring("dd-MM-yyyy-hh-mm-ss")
-$dirName = New-Item -ItemType directory -Path "c:\windows\system32\dhcp\backup\$FileName" -Force
+$dirName = New-Item -ItemType directory -Path "$Bkppth\$FileName" -Force
 Get-ChildItem "c:\windows\system32\dhcp\backup"| Where-Object{$_.Name -notlike "sched*"} | Copy-Item -Destination $dirName -Recurse -Force
+}
+
+if((($bkpint.gettype().Name) -eq 'int32') -and (Test-Path $Bkppth)){
+&$backup
+}
+
+if(-not(Test-Path $Bkppth))
+{
+Write-Host $Bkppth unreachable... Check DHCP settings and $key }
+
+elseif((($bkpint.gettype().Name) -ne 'int32'))
+{
+Write-Host Backup Interval Missing... Check $key}
